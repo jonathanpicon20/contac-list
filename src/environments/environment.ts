3bf3a2c19f373f4e7d6113104a463222a1d005ce
JP <jonathan.picon20@gmail.com>
2024-06-11 13:50:45 -0500

    Inicio de la App

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'contacs-list',
+    appId: '1:283029190989:web:795d887285546679c21f04',
+    storageBucket: 'contacs-list.appspot.com',
+    apiKey: 'AIzaSyDkUhXg_VOJdccJagPuEIqiqAuemkAi3HM',
+    authDomain: 'contacs-list.firebaseapp.com',
+    messagingSenderId: '283029190989',
+  },
+};
